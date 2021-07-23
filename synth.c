e06804b16cb9a7f16fd28f08b5bb5c61368bb622
Marshal Horn <kamocat@gmail.com>
2021-07-23 04:17:33 -0700

    Nice-sounding organ tone

--- synth.c
+++ synth.c
@@ -1,18 +1,37 @@
 #include "synth.h"
-#define N 2
+#define N 9
 
-int16_t xp, yp, dt;
+int8_t amplitudes[N]  = { 25, 15, 25, 15, 10, 2, 5, 1,1 };
+int8_t frequencies[N] = { 2,3,4,5,6,7,8,9,10 };
+int8_t p[N][2];
 
 void synth_init(void){
-  // xp/dt >= 5/2
-  xp = 32000;
-  yp = 0;
-  dt = 30;
+  for(int i=0; i<N; ++i){
+    p[i][0]=120;
+    p[i][1]=0;
+  }
+  
 }
 
-int16_t synth(void){
-  yp += xp / dt;
-  xp -= yp / dt;
-  return xp;
+int8_t synth(void){
+  int16_t sum = 0;
+  int16_t m;
+  for(int i=0; i<N; ++i){
+    m = minsky(&p[i][0],&p[i][1],frequencies[i]);
+    m *= amplitudes[i];
+    sum += m;
+  }
+  return sum>>7;
 }
 
+int8_t minsky(int8_t *x, int8_t *y, int8_t dt){
+  *y += mul8(*x, dt);
+  *x -= mul8(*y, dt);
+  return *x;
+}
+
+int8_t mul8(int8_t a, int8_t b){
+  int16_t tmp = a;
+  tmp *= b;
+  return tmp>>7;
+}
--- synth.h
+++ synth.h
@@ -1,5 +1,17 @@
 #include <stdint.h>
 void synth_init(void);
 
-int16_t synth(void);
+/** Outputs a tone point-by-point.
+ * Uses internal state for storage, in classic embedded fasion
+ */
+int8_t synth(void);
 
+/** Computes the next step in the circle
+ * assumes x and y range between -120 and 120.
+ * Works best when dx is at least 2.
+ */
+int8_t minsky(int8_t *x, int8_t *y, int8_t dt);
+
+/** Multiplies two 8-bit integers as if they were 1.7 fixed-point
+ */ 
+int8_t mul8(int8_t a, int8_t b);
