00a8d92f534c43c97bd22884c5280c88ccae6414
Marshal Horn <kamocat@gmail.com>
2021-07-23 04:14:55 -0700

    Switched to 8-bit audio

--- synth_tb.cpp
+++ synth_tb.cpp
@@ -4,6 +4,15 @@
 #include "synth.h"
 using namespace std;
 
+
+int test_mul8(int8_t a, int8_t b, int8_t y){
+  int tmp = mul8(a,b);
+  bool pass = y==tmp;
+  std::cout << int(a)<< "*" << int(b)<< "=" << tmp 
+    << "..."<<(pass?"PASS":"FAIL")<<std::endl;
+  return !pass;
+}
+
 namespace little_endian_io
 {
   template <typename Word>
@@ -18,6 +27,10 @@ using namespace little_endian_io;
 
 int main()
 {
+  test_mul8(127,127,126);
+  test_mul8(-127,-127,126);
+  test_mul8(127,0,0);
+  test_mul8(64,2,1);
   ofstream f( "example.wav", ios::binary );
 
   // Write the file headers
@@ -26,9 +39,9 @@ int main()
   write_word( f,      1, 2 );  // PCM - integer samples
   write_word( f,      1, 2 );  // one channel (mono file)
   write_word( f,  44100, 4 );  // samples per second (Hz)
-  write_word( f,  88200, 4 );  // (Sample Rate * BitsPerSample * Channels) / 8
-  write_word( f,      2, 2 );  // data block size (size of two integer samples, one for each channel, in bytes)
-  write_word( f,     16, 2 );  // number of bits per sample (use a multiple of 8)
+  write_word( f,  44100, 4 );  // (Sample Rate * BitsPerSample * Channels) / 8
+  write_word( f,      1, 2 );  // data block size
+  write_word( f,      8, 2 );  // number of bits per sample (use a multiple of 8)
 
   // Write the data chunk header
   size_t data_chunk_pos = f.tellp();
@@ -41,7 +54,7 @@ int main()
   int N = hz * seconds;  // total number of samples
   for (int n = 0; n < N; n++)
   {
-    write_word( f, synth(), 2 );
+    write_word( f, 127+synth(), 1 );
   }
 
   // (We'll need the final file size fot fix the chunk sizes above)
