4f22320a69eff5537a65c0c51d2ad89d7cd63eb9
Marshal Horn <kamocat@gmail.com>
2021-07-28 12:48:10 -0700

    Made a nice bell sound. Need to try other algorithms

--- fm.m
+++ fm.m
@@ -1,13 +1,13 @@
 pitch = 440*2*pi;
 fs = 44100;
-seconds = 3;
+seconds = 4;
 n = fs*seconds;
 wf = seconds*pitch;
-rate = 5;
-level = 0.6;
+rate = 2.9;
+level = 5;
 
 w = linspace(0,wf,n);
-x = linspace(0,1,n) .* sin(linspace(0,wf*rate,n));
+x = logspace(log10(level),-1,n) .* sin(linspace(0,wf*rate,n));
 y = sin(w + x);
 
 audiowrite("synth.wav", y, fs, 'BitsPerSample',8);
