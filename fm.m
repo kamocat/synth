37cae8d158c7e8149fb848825b47d8ffb735aa25
Marshal Horn <kamocat@gmail.com>
2021-07-28 18:46:29 -0700

    Improved bell sound to sweetie's favorite

--- fm.m
+++ fm.m
@@ -1,13 +1,15 @@
-pitch = 440*2*pi;
+pitch = 657*2*pi;
 fs = 44100;
 seconds = 4;
 n = fs*seconds;
 wf = seconds*pitch;
-rate = 2.9;
+rate = 3.30;
 level = 5;
 
 w = linspace(0,wf,n);
 x = logspace(log10(level),-1,n) .* sin(linspace(0,wf*rate,n));
 y = sin(w + x);
+y = y .* logspace(0,-2,n);
 
 audiowrite("synth.wav", y, fs, 'BitsPerSample',8);
+unix("mplayer synth.wav");
