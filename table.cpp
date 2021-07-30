53bd8345079b4af90530645f8c1750e23414c59e
Marshal Horn <kamocat@gmail.com>
2021-07-29 20:34:00 -0700

    fixed hiccup in cosine wave

--- table.cpp
+++ table.cpp
@@ -1,12 +1,12 @@
 #include "table.hpp"
 
 SineTable::SineTable(){
-  len = 65;
+  len = 64;
   const int8_t step = 3;
   const int8_t bits = 2; // Bit space of step
   const int8_t pad = 8 - bits; // Extra padding for better precision
   int16_t x = 127<<pad;
-  int16_t y = 0;
+  int16_t y = 5<<pad; // Slight phase advance to fix wave symmetry
   data = new int8_t[len];
   for(uint8_t i = 0; i<len; ++i){
     y += (x*step)>>7;
@@ -24,8 +24,8 @@ int8_t SineTable::lookup(int8_t index){
   int8_t val;
   if(index<0)
     index = ~index; // Even symmetry around 0
-  if(index > 0x40)
-    val = -data[0x7F - index]; // Odd symmetry around pi/2
+  if(index >= 0x40)
+    val = -data[0x80 - index]; // Odd symmetry around pi/2
   else
     val = data[index];
   return val;
